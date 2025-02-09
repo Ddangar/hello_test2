feccae8360c6553892c3fede74d13dc7618de0b6
Ddangar <deepakdancingdude@gmail.com>
2025-02-09 13:25:45 +0000

    1 test code

---
+++ hello.py
@@ -0,0 +1,10 @@
+  GNU nano 4.8                                                       index.html                                                        Modified  
+html>
+<head>
+  <title><b>Practial Assigment B!</b> </title>
+</head>
+<body>
+  <p> Hey I'm <b>Deepak sinh</b> giving this Practial assigment B on <input 09/02/25> this's my website on an Ubuntu Server! Hope you like it.
+</body>
+</html>
+
